36db1239a0278ce4047a259569c39c0affd9ba84
Harsh Vekariya <harshvekariya910@gmail.com>
2025-04-26 22:29:03 +0530

    ok

--- app.py
+++ app.py
@@ -167,3 +167,28 @@ elif app_mode == "Leather Anomaly Detection":
 
             st.success(f"**Prediction:** {class_name}")
             st.info(f"**Confidence Score:** {confidence_score:.2f}")
+
+
+# Add custom CSS for background image
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://images.unsplash.com/photo-1563832528262-15e2bca87584?q=80&w=2019&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        # background-image: url("https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        # opacity: 0.5;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
